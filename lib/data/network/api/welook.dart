d81412b6bc36712a817b06d9a4453de6e9717b4e
RockerFlower <git@wong.so>
2022-08-24 21:09:04 +0800

    ✨ Add Welook's latest API on moments

--- lib/data/network/api/welook.dart
+++ lib/data/network/api/welook.dart
@@ -17,4 +17,90 @@ class WelookApi {
       rethrow;
     }
   }
+
+  Future<Response> getMomentsOfEvent(
+    int eventID, {
+    int limit = 20,
+    int offset = 0,
+    String sort = 'asc',
+  }) async {
+    try {
+      final Response response = await dioClient.get(
+        WelookConstant.getMementsOfEvent(
+          eventID,
+          limit: limit,
+          offset: offset,
+          sort: sort,
+        ),
+      );
+      return response;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Response> getMomentsOfAddress(
+    String address, {
+    int limit = 20,
+    int offset = 0,
+    String sort = 'asc',
+  }) async {
+    try {
+      final Response response = await dioClient.get(
+        WelookConstant.getMomentsOfAddress(
+          address,
+          limit: limit,
+          offset: offset,
+          sort: sort,
+        ),
+      );
+      return response;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Response> getMomentsOfAddressInEvent(
+    String address,
+    int eventID, {
+    int limit = 20,
+    int offset = 0,
+    String sort = 'asc',
+  }) async {
+    try {
+      final Response response = await dioClient.get(
+        WelookConstant.getMomentsOfAddressInEvent(
+          address,
+          eventID,
+          limit: limit,
+          offset: offset,
+          sort: sort,
+        ),
+      );
+      return response;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Response> getMoment(
+    int momentID, {
+    int limit = 20,
+    int offset = 0,
+    String sort = 'asc',
+  }) async {
+    try {
+      final Response response = await dioClient.get(
+        WelookConstant.getMoment(
+          momentID,
+          limit: limit,
+          offset: offset,
+          sort: sort,
+        ),
+      );
+      return response;
+    } catch (e) {
+      rethrow;
+    }
+  }
 }
