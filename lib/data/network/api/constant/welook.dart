40f6fcd94829e3b09446936b4a7f75012a5d8661
RockerFlower <git@wong.so>
2022-08-24 20:37:54 +0800

    👽️ Update Welook API to v2

--- lib/data/network/api/constant/welook.dart
+++ lib/data/network/api/constant/welook.dart
@@ -1,7 +1,11 @@
+import 'package:poapin/secrets.dart';
+
 class WelookConstant {
   WelookConstant._();
 
-  static const String welookUrl = "https://welook.tech/api/v1";
+  static const String welookUrl = "https://welook.tech/api/v2";
+
+  static const String welookAPIKey = Secrets.welookAPIKEY;
 
   /// ------------------------------------------------------------
   /// Endpoints
@@ -9,5 +13,5 @@ class WelookConstant {
 
   /// Check if there are available Welook moments for
   /// a certain POAP and the exact amount.
-  static String momentCount(int eventID) => '/moments/total/$eventID';
+  static String momentCount(int eventID) => '/moments/$eventID/total';
 }
