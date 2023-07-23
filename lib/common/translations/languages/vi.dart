4eda0b02b06e433d2a2bd2ddad5bda26fda9cd95
RockerFlower <git@wong.so>
2023-07-23 10:10:46 +0900

    🌐 Update Vietnamese translation

--- lib/common/translations/languages/vi.dart
+++ lib/common/translations/languages/vi.dart
@@ -7,148 +7,149 @@ class ViTranslations {
         'POAPin': 'POAPin',
 
         /// Tabs
-        // Strings.home: 'Home',
-        // Strings.watchlist: 'Watchlist',
-        // Strings.me: 'Me',
+        Strings.home: 'Trang chủ',
+        Strings.watchlist: 'Danh sách theo dõi',
+        Strings.me: 'Tôi',
 
         /// Home(Initial state)
-        // Strings.enjoy: 'Enjoy a life with POAP',
-        // Strings.setAddress:
-        //     'Set your address and you\'ll see your POAPs on the home page',
-        // Strings.setEthAddress: 'Set ETH address',
-        // Strings.ethAddressOrEns: 'ETH address or ENS',
-        // Strings.invalidAddress: 'Invalid address',
+        Strings.enjoy: 'Hưởng thụ cuộc sống với POAP',
+        Strings.setAddress:
+            'Thiết lập địa chỉ của bạn và bạn sẽ thấy POAPs trên trang chủ',
+        Strings.setEthAddress: 'Đặt địa chỉ ETH',
+        Strings.ethAddressOrEns: 'Địa chỉ ETH hoặc ENS',
+        Strings.invalidAddress: 'Địa chỉ không hợp lệ',
 
         /// Home(empty)
-        // Strings.noPOAP:
-        //     'You don\'t appear to have any POAP right now.\nBut it\'s not too late!\n\nBegin learning about POAP by creating a POAP.',
+        Strings.noPOAP:
+            'Bạn dường như không có POAP nào ngay bây giờ.\nNhưng không phải quá muộn!\n\nBắt đầu tìm hiểu về POAP bằng cách tạo ra một POAP.',
 
         /// Home(available)
-        // Strings.followers: 'followers',
-        // Strings.followings: 'followings',
-        // Strings.events: 'Events',
-        // Strings.poaps: 'POAPs',
-        // Strings.growth: 'growth',
-        // Strings.monthly: 'monthly',
-        // Strings.filterHint: 'Filter by name, description, location, etc.',
+        Strings.followers: 'người theo dõi',
+        Strings.followings: 'đang theo dõi',
+        Strings.events: 'Sự kiện',
+        Strings.poaps: 'POAPs',
+        Strings.growth: 'tăng trưởng',
+        Strings.monthly: 'hàng tháng',
+        Strings.filterHint: 'Lọc theo tên, mô tả, vị trí, v.v.',
         // --- Prefs
-        // Strings.visibility: 'Visibility',
-        // Strings.hideDuplicates: 'Hide Duplicates',
-        // Strings.showAll: 'Show all',
-        // Strings.sort: 'Sort',
-        // Strings.newest: 'Newest',
-        // Strings.oldest: 'Oldest',
-        // Strings.shape: 'Shape',
-        // Strings.square: 'Square',
-        // Strings.round: 'Round',
-        // Strings.layout: 'Layout',
-        // Strings.grid: 'Grid',
-        // Strings.list: 'List',
-        // Strings.timeline: 'Timeline',
+        Strings.visibility: 'Khả năng nhìn thấy',
+        Strings.hideDuplicates: 'Ẩn bản sao',
+        Strings.showAll: 'Hiển thị tất cả',
+        Strings.sort: 'Sắp xếp',
+        Strings.newest: 'Mới nhất',
+        Strings.oldest: 'Cũ nhất',
+        Strings.shape: 'Hình dạng',
+        Strings.square: 'Vuông',
+        Strings.round: 'Tròn',
+        Strings.layout: 'Bố cục',
+        Strings.grid: 'Lưới',
+        Strings.list: 'Danh sách',
+        Strings.timeline: 'Dòng thời gian',
         // --- Filter
-        // Strings.filter: 'Filter',
-        // Strings.filterTitle: 'Title',
-        // Strings.filterDescription: 'Description',
-        // Strings.filterCountry: 'Country',
-        // Strings.filterTag: 'Tag',
-        // Strings.filterChain: 'Chain',
-        // Strings.filterClearAll: 'Clear ALL',
+        Strings.filter: 'Bộ lọc',
+        Strings.filterTitle: 'Tiêu đề',
+        Strings.filterDescription: 'Mô tả',
+        Strings.filterCountry: 'Quốc gia',
+        Strings.filterTag: 'Thẻ',
+        Strings.filterChain: 'Chuỗi',
+        Strings.filterClearAll: 'Xóa tất cả',
 
         /// Home(edit mode)
-        // Strings.editMode: 'Edit POAPs',
-        // Strings.editTags: 'Edit tags',
-        // Strings.newTag: 'New Tag',
-        // Strings.newTagHint: 'new tag for event',
+        Strings.editMode: 'Chỉnh sửa POAPs',
+        Strings.editTags: 'Chỉnh sửa thẻ',
+        Strings.newTag: 'Thẻ mới',
+        Strings.newTagHint: 'thẻ mới cho sự kiện',
 
         /// Moments
-        // Strings.momentsDesc: '''Your POAP collection is your digital ID.
-        //
-        // It shows the world who you are: where you’ve been, what you achieved and who you’ve met.
-        //
-        // Moments is a way to add real value to your digital bookmark by saving real-life instances in a POAP, forever.
-        // ''',
-        //Strings.uploadMoments: 'Upload moments on welook.io',
+        Strings.momentsDesc:
+            '''Bộ sưu tập POAP của bạn là ID kỹ thuật số của bạn.
+        
+        Nó cho thế giới biết bạn là ai: nơi bạn đã đến, những gì bạn đã đạt được và ai bạn đã gặp.
+        
+        Moments là cách để thêm giá trị thực sự cho dấu trang kỹ thuật số của bạn bằng cách lưu trữ những trường hợp trong đời thực trong một POAP, mãi mãi.
+        ''',
+        Strings.uploadMoments: 'Tải lên những khoảnh khắc trên welook.io',
 
         /// GitPOAP
-        // Strings.gitPOAPDesc:
-        //             '''A GitPOAP is a regular POAP (with an extra spin) that is minted through the GitPOAP platform for specified meaningful contribution to open source projects. Repo owners automatically distribute GitPOAPs to their contributors as recognition of their work.
-        // ''',
-        // Strings.gitPOAPStart: 'Contribute to POAPin',
+        Strings.gitPOAPDesc:
+            '''Một GitPOAP là một POAP thông thường (với một chút xoay) được khai thác thông qua nền tảng GitPOAP cho các đóng góp có ý nghĩa đặc biệt cho các dự án nguồn mở. Chủ sở hữu Repo tự động phân phối GitPOAPs cho những người đóng góp của họ như một sự công nhận công việc của họ.
+        ''',
+        Strings.gitPOAPStart: 'Đóng góp cho POAPin',
 
         /// Watchlist(Initial state)
-        // Strings.watchlistHint:
-        //     'You can add some addresses you are interested in here.',
-        // Strings.addCollection: 'Add your first collection',
+        Strings.watchlistHint:
+            'Bạn có thể thêm một số địa chỉ bạn quan tâm ở đây.',
+        Strings.addCollection: 'Thêm bộ sưu tập đầu tiên của bạn',
 
         /// Watchlist(available)
-        // Strings.follow: 'Follow',
-        // Strings.unfollow: 'Unfollow',
+        Strings.follow: 'Theo dõi',
+        Strings.unfollow: 'Bỏ theo dõi',
 
         /// Me
-        // Strings.connectWalletHint: 'Sign in with browser & connect wallet.',
+        Strings.connectWalletHint: 'Đăng nhập bằng trình duyệt và kết nối ví.',
 
         /// Profile
-        // Strings.profile: 'Profile',
-        // Strings.dangerZone: 'Danger Zone',
-        // Strings.deleteAccount: 'Delete account',
-        // Strings.deleteAccountDesc:
-        //     'Are you certain you want to delete your account?\n\nThis operation cannot be undone.',
-        // Strings.deleteAccountConfirm: 'Delete',
+        Strings.profile: 'Hồ sơ',
+        Strings.dangerZone: 'Vùng nguy hiểm',
+        Strings.deleteAccount: 'Xóa tài khoản',
+        Strings.deleteAccountDesc:
+            'Bạn có chắc chắn muốn xóa tài khoản của mình?\n\nThao tác này không thể hoàn tác.',
+        Strings.deleteAccountConfirm: 'Xóa',
 
         /// Tags
-        // Strings.tags: 'Tags',
-        // Strings.manageTags: 'Manage your tags',
-        // Strings.noTags: 'No tags yet',
-        // Strings.addTag: 'New Tag',
-        // Strings.editTagHint: 'choose from above\nor',
+        Strings.tags: 'Thẻ',
+        Strings.manageTags: 'Quản lý thẻ của bạn',
+        Strings.noTags: 'Chưa có thẻ',
+        Strings.addTag: 'Thêm thẻ mới',
+        Strings.editTagHint: 'chọn từ trên\nhoặc',
 
         /// Settings
-        // Strings.settings: 'Settings',
-        // Strings.settingsDesc: 'Notifications, data, etc.',
-        // Strings.notification: 'Notification',
-        // Strings.notificationEmail: 'Email',
-        // Strings.notificationEmailDesc: 'Get email notifications',
-        // Strings.notificationApp: 'App',
-        // Strings.notificationAppDesc: 'Get app notifications',
-        // Strings.notificationAppThisDevice: 'This device',
-        // Strings.notificationAppDescMore: 'Get app notifications',
-        // Strings.notificationBrowser: 'Browser',
-        // Strings.notificationBrowserDesc: 'Get browser notifications',
-        // Strings.notificationPush: 'Push',
-        // Strings.notificationPushDesc: 'Get push notifications',
-        // Strings.general: 'General',
-        // Strings.language: 'Language',
-        // Strings.contributeLanguage: '🙇 Contribute more translations 🔗',
-        // Strings.data: 'General',
-        // Strings.cache: 'Clear all cache',
-        // Strings.cacheDesc: 'If there are problems with app, please try it.',
-        // Strings.about: 'About',
+        Strings.settings: 'Cài đặt',
+        Strings.settingsDesc: 'Thông báo, dữ liệu, v.v.',
+        Strings.notification: 'Thông báo',
+        Strings.notificationEmail: 'Email',
+        Strings.notificationEmailDesc: 'Nhận thông báo qua email',
+        Strings.notificationApp: 'Ứng dụng',
+        Strings.notificationAppDesc: 'Nhận thông báo từ ứng dụng',
+        Strings.notificationAppThisDevice: 'Thiết bị này',
+        Strings.notificationAppDescMore: 'Nhận thông báo từ ứng dụng',
+        Strings.notificationBrowser: 'Trình duyệt',
+        Strings.notificationBrowserDesc: 'Nhận thông báo từ trình duyệt',
+        Strings.notificationPush: 'Đẩy',
+        Strings.notificationPushDesc: 'Nhận thông báo đẩy',
+        Strings.general: 'Chung',
+        Strings.language: 'Ngôn ngữ',
+        Strings.contributeLanguage: '🙇 Đóng góp thêm bản dịch 🔗',
+        Strings.data: 'Dữ liệu',
+        Strings.cache: 'Xóa tất cả bộ nhớ cache',
+        Strings.cacheDesc: 'Nếu có sự cố với ứng dụng, hãy thử nó.',
+        Strings.about: 'Giới thiệu',
 
         /// Account
-        // Strings.signIn: 'Sign in',
-        // Strings.logOut: 'Log out',
-        // Strings.signinApple: 'Sign in with Apple',
-        // Strings.signinGoogle: 'Continue with Google',
-        // Strings.tos:
-        //     'By continuing, you agree to our Terms of Service and Privacy Policy',
+        Strings.signIn: 'Đăng nhập',
+        Strings.logOut: 'Đăng xuất',
+        Strings.signinApple: 'Đăng nhập với Apple',
+        Strings.signinGoogle: 'Tiếp tục với Google',
+        Strings.tos:
+            'Bằng cách tiếp tục, bạn đồng ý với Điều khoản Dịch vụ và Chính sách Bảo mật của chúng tôi',
 
         /// Common
-        // Strings.done: 'Done',
-        // Strings.alert: 'Alert',
-        // Strings.error: 'Error',
-        // Strings.learnMore: 'learn more',
+        Strings.done: 'Hoàn thành',
+        Strings.alert: 'Cảnh báo',
+        Strings.error: 'Lỗi',
+        Strings.learnMore: 'tìm hiểu thêm',
         // --- Months
-        // Strings.january: 'January',
-        // Strings.february: 'February',
-        // Strings.march: 'March',
-        // Strings.april: 'April',
-        // Strings.may: 'May',
-        // Strings.june: 'June',
-        // Strings.july: 'July',
-        // Strings.august: 'August',
-        // Strings.september: 'September',
-        // Strings.october: 'October',
-        // Strings.november: 'November',
-        // Strings.december: 'December',
+        Strings.january: 'Tháng Một',
+        Strings.february: 'Tháng Hai',
+        Strings.march: 'Tháng Ba',
+        Strings.april: 'Tháng Tư',
+        Strings.may: 'Tháng Năm',
+        Strings.june: 'Tháng Sáu',
+        Strings.july: 'Tháng Bảy',
+        Strings.august: 'Tháng Tám',
+        Strings.september: 'Tháng Chín',
+        Strings.october: 'Tháng Mười',
+        Strings.november: 'Tháng Mười Một',
+        Strings.december: 'Tháng Mười Hai',
       };
 }
