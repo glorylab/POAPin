9cc93931750d24adf6c3fb056a6b2c721477d68f
RockerFlower <git@wong.so>
2023-10-21 12:49:35 +0900

    🚸 Improve address input

--- lib/ui/pages/home/components/button.set_eth_address.dart
+++ lib/ui/pages/home/components/button.set_eth_address.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:poapin/common/translations/strings.dart';
+import 'package:poapin/ui/pages/home/controller.dart';
+import 'package:poapin/ui/pages/home/controllers/island.dart';
 import 'package:poapin/ui/pages/me/controller.dart';
 import 'package:poapin/util/show_input.dart';
 
@@ -10,46 +12,54 @@ class SetETHAddressButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return RawMaterialButton(
-      elevation: 1,
-      highlightColor: Theme.of(context).primaryColorLight,
-      shape: const RoundedRectangleBorder(
-        side: BorderSide(color: Color(0x116534FF), width: 4),
-        borderRadius: BorderRadius.all(
-          Radius.circular(8),
+    return GetBuilder<HomeController>(
+      builder: (c) => RawMaterialButton(
+        elevation: 1,
+        highlightColor: Theme.of(context).primaryColorLight,
+        shape: const RoundedRectangleBorder(
+          side: BorderSide(color: Color(0x116534FF), width: 4),
+          borderRadius: BorderRadius.all(
+            Radius.circular(8),
+          ),
         ),
-      ),
-      fillColor: Colors.white,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const SizedBox(
-            width: 12,
-          ),
-          const Icon(Icons.auto_awesome),
-          const SizedBox(
-            width: 16,
-          ),
-          Container(
-            alignment: Alignment.center,
-            child: Text(
-              strSetEthAddress,
-              maxLines: 1,
-              style: GoogleFonts.courierPrime(
-                color: Theme.of(context).primaryColorDark,
-                fontSize: 20,
+        fillColor: Colors.white,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(
+              width: 12,
+            ),
+            const Icon(Icons.auto_awesome),
+            const SizedBox(
+              width: 16,
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                strSetEthAddress,
+                maxLines: 1,
+                style: GoogleFonts.courierPrime(
+                  color: Theme.of(context).primaryColorDark,
+                  fontSize: 20,
+                ),
               ),
             ),
-          ),
-        ],
-      ),
-      onPressed: () async {
-        InputHelper.showBottomInput(
+          ],
+        ),
+        onPressed: () async {
+          Get.put(IslandController());
+          InputHelper.showAccountInput(
             context,
-            strEthAddressOrEns,
-            Get.find<MeController>().addressController,
-            Get.find<MeController>().onSubmit);
-      },
+            'poap.eth / 0x00...',
+            c.addressController,
+            c.onAddressSubmit,
+          ).then((value) {
+            if (value == 'close') {
+              c.setIslandExpanded(false);
+            }
+          });
+        },
+      ),
     );
   }
 }
