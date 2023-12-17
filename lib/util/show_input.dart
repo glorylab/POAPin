be69adb8ad9b754fdbd0356d8920b69aa8e51fa6
RockerFlower <git@wong.so>
2023-12-17 23:52:11 +0900

    🎨 Add ShowContentInput

--- lib/util/show_input.dart
+++ lib/util/show_input.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:poapin/res/colors.dart';
 import 'package:poapin/ui/components/input.account.dart';
 import 'package:poapin/ui/components/input.address.dart';
+import 'package:poapin/ui/components/input.content.dart';
 
 class InputHelper {
   static showBottomInput(BuildContext context, String hint,
@@ -71,6 +72,56 @@ class InputHelper {
                   bottom: 0,
                   top: 0,
                   child: AccountInputPage(
+                    controller: controller,
+                    onEditingComplete: onEditingComplete,
+                    hint: hint,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      useSafeArea: false,
+      transitionDuration: const Duration(milliseconds: 120),
+      transitionCurve: Curves.easeInExpo,
+    ).then((value) => value);
+  }
+
+  static showContentInput(BuildContext context, String hint,
+      TextEditingController controller, Function() onEditingComplete) {
+    return Get.dialog(
+      Material(
+        type: MaterialType.transparency,
+        child: GestureDetector(
+          onTap: () {
+            Get.back();
+          },
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            resizeToAvoidBottomInset: true,
+            body: Stack(
+              children: [
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  top: 0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [Colors.white, PColor.background],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  top: 0,
+                  child: ContentInputPage(
                     controller: controller,
                     onEditingComplete: onEditingComplete,
                     hint: hint,
