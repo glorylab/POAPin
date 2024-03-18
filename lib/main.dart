21bb6004a4ba0c32536e49d429d746e9df80c82d
Kira <kira@glorylab.xyz>
2024-03-18 23:41:44 +0900

    🏗️ Wrap application in ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:poapin/common/constants.dart';
@@ -116,7 +117,7 @@ Future<void> main() async {
       });
     }
   });
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
